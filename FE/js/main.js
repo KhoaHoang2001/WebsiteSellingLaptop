1721ef466204853292a5453b06feec6835e1b4c8
SELQThang <selqthang@gmail.com>
2021-04-22 17:46:50 +0700

    add validation login form

--- FE/js/main.js
+++ FE/js/main.js
@@ -1,7 +1,30 @@
-function myFunction(e) {
-    var elems = document.querySelectorAll(".active");
-    [].forEach.call(elems, function(el) {
-      el.classList.remove("active");
-    });
-    e.target.className = "active";
+var validate = new Validation();
+
+function DomID(id) {
+  var element = document.getElementById(id);
+  return element;
+}
+
+
+function DangNhap() {
+  var tenDangNhap = DomID("tenDangNhap").value;
+  var matKhau = DomID("matKhau").value;
+
+  if (KiemTraDauVaoRong("tenDangNhap", tenDangNhap) == true) {
+      console.log("erro");
   }
+  if (KiemTraDauVaoRong("matKhau", matKhau) == true) {
+  }
+}
+
+function KiemTraDauVaoRong(ID, value) {
+  if (validate.KiemTraRong(value) == true) {
+      DomID(ID).style.borderColor = "red";
+      DomID(ID).placeholder = "Không được rỗng";
+      return true;
+  }
+  else{
+    DomID(ID).style.borderColor = "";
+    return false;
+  }
+}
